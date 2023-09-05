5843d9a5f729fa72ffa5098e5ac5e1ec92c44698
forsant <139800319+forsant@users.noreply.github.com>
2023-09-05 16:00:37 +0200

    Nouveau fichier JS

---
+++ script1.js
@@ -0,0 +1,13 @@
+var nombre_inc =10;
+var nombre_entre;
+nombre_entre = prompt("quel est le nombre inconu ?");
+
+if(nombre_entre > nombre_inc){
+    alert("trop grand");}
+
+if(nombre_entre < nombre_inc){
+        alert("trop petit");}  
+
+if (nombre_entre == nombre_inc){
+    alert("c'est top");
+}
---
+++ script2.js
@@ -0,0 +1,9 @@
+var age_limite = 18;
+var age_entre;
+age_entre = prompt("Quel est votre age ? : ");
+
+if(age_entre <= age_limite){
+alert("Bienvenue");}
+else{
+alert("NON AUTORISE VOUS AVEC PLUS DE "+age_limite+" ANS !");}
+    
