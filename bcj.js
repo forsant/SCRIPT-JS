b7f4e6cc8569c56d738afff4e809953a1e63897d
forsant <139800319+forsant@users.noreply.github.com>
2023-09-05 16:26:48 +0200

    fichier js

---
+++ bcj.js
@@ -0,0 +1,5 @@
+var n;
+n = prompt("veuillez entrer un nombre n=");
+for(i=1; i<=20 ;i=i+1){ // (i=i+1)incrémenter de 1 a 1 ou on écrit (i++)
+    document.write(n*i + "<br />")
+}
--- script2.js
+++ script2.js
@@ -2,8 +2,9 @@ var age_limite = 18;
 var age_entre;
 age_entre = prompt("Quel est votre age ? : ");
 
+// Test if et sinon
 if(age_entre <= age_limite){
 alert("Bienvenue");}
 else{
 alert("NON AUTORISE VOUS AVEC PLUS DE "+age_limite+" ANS !");}
-    
+     
